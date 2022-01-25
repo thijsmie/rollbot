d63bcfc3ff793ee99b847b8ae2b133e734e04ee0
Thijs Miedema <mail@tmiedema.com>
2022-01-25 21:01:56 +0100

    No typing

--- rollbot/bot/handlers.py
+++ rollbot/bot/handlers.py
@@ -32,18 +32,17 @@ async def distribution(context: Context, roll: str):
     env = get_var_env(context)
 
     await context.defer()
-    async with context.typing():
-        try:
-            png = await bake_distribution(roll, env)
-            await context.respond(
-                file=discord.File(png, filename=f"{secrets.token_urlsafe(8)}.png")
-            )
-            return
-        except BakingError as e:
-            result = e.args[0]
-        except:
-            result = "Server error"
-        await context.respond(result)
+    try:
+        png = await bake_distribution(roll, env)
+        await context.respond(
+            file=discord.File(png, filename=f"{secrets.token_urlsafe(8)}.png")
+        )
+        return
+    except BakingError as e:
+        result = e.args[0]
+    except:
+        result = "Server error"
+    await context.respond(result)
 
 
 async def varlist(context: Context, roll: str):
